44bb0c0a145ebf5ad6933d9e1e34d7bad3e04d7e
Milu <mloeda@hotmail.com>
2026-04-23 18:40:30 -0300

    Delete Clase 11 directory

--- Clase 11/ejercicio 16.py
+++
@@ -1,6 +0,0 @@
-nombre = input("Nombre del producto: ")
-precio = float(input("Precio unitario: "))
-unidades = int(input("Número de unidades: "))
-
-total = precio * unidades
-print(f"{nombre} {precio:09.2f} {unidades:03d} {total:011.2f}")
